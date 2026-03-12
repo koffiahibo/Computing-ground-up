3ba9b6164255cd0f1e480a1aea691989c332b738
koffiahibo <koffiahibo2024@gmail.com>
2026-03-12 16:36:36 +0000

    added more gates descriptions

--- Computer-Architecture/CPU/logic_gates.py
+++ Computer-Architecture/CPU/logic_gates.py
@@ -19,33 +19,32 @@ def Mux(a, b, sel):
     return Or(And(a, Not(sel)), And(sel,b))
 def DMux(sel, Inp):
     return (And(sel,Inp ),And(Not(sel),Inp))
-def Not16(bits16_list): # a list of 16 bits
-    if len(bits16_list) != 16:
-        raise ValueError(f"Expected 16 bits, got {len(bits16_list)}")
-    Not_bus = []
-    for bit in bits16_list:
-        Not_bus.append(Not(bit))
-    return Not_bus
-def And16(bits16_bus1, bits16_bus2): # 16-lists of bits
-    if len(bits16_bus1) != 16 or len(bits16_bus2) != 16:
-        raise ValueError(t"Expected 16 bits, got {len(bits16_bus1)}, {len(bits16_bus2)}")
-    And_bus = []
-    for i in range(15):
-        And_bus.append(And(bits16_bus1[i],bits16_bus2[i]))
-    return And_bus
-def Or16(a, b):
+def Not16(a16): # a list of 16 bits
+    if len(a16) != 16:
+        raise ValueError(f"Expected 16 bits, got {len(a16)}")
+    return [Not(a16[i]) for i in range(16)]
+def And16(a16, b16): # 16-lists of bits
+    if len(a16) != 16 or len(b16) != 16:
+        raise ValueError(f"Expected 16 bits, got {len(a16)}, {len(b16)}")
+    return [And(a16[i],b16[i]) for i in range(16)]
+def Or16(a16, b16):
+    if len(a16) != 16 or len(b16) != 16:
+        raise ValueError(f"Expected 16 bits, got {len(a16)}, {len(b16)}")
+    return [Or(a16[i],b16[i]) for i in range(16)]
+def Mux16(a16, b16, sel):
+    if len(a16) != 16 or len(b16) != 16:
+        raise ValueError(f"Expected 16 bits, got {len(a16)}, {len(b16)}")
+    return [Mux(a16[i], b16[i], sel) for i in range(16)]
+def Or8Way(a8):
+    pre_Or = 0
+    for i in range(8):
+        pre_Or = Or(a8[i],pre_Or)
+    return pre_Or
+
+def Mux4Way16(a16_4, sel): #4-list of 16-lists
     pass
 
-def Mux16(a, b, sel):
-    pass
-
-def Or8Way(a):
-    pass
-
-def Mux4Way16(a, b, c, d, sel):
-    pass
-
-def Mux8Way16(a, b, c, d, e, f, g, h, sel):
+def Mux8Way16(a16, sel):
     pass
 
 def DMux4Way(inp, sel):
@@ -53,4 +52,13 @@ def DMux4Way(inp, sel):
 
 def DMux8Way(inp, sel):
     pass
-print(f" {Xor(0,0)}, {Xor(1,0)}, {Xor(0,1)}, {Xor(1,1)}")
+a1 =[1,0,1,0,1,0,1,0]
+a = [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0]
+b = [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1]
+print(Or8Way(a1))
+
+print(Not16(a))      # expected: [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1]
+print(And16(a,b))    # expected: all zeros
+print(Or16(a,b))     # expected: all ones
+print(Mux16(a,b,0))  # expected: a
+print(Mux16(a,b,1))  # expected: b
